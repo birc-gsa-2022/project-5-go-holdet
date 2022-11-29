ead4e99726923302b3a74ca586716f31491f90ed
au633096 <1851-au633096@users.noreply.gitlab.au.dk>
2022-11-29 13:56:24 +0100

    no more unused variables >:(

--- shared/sais.go
+++ shared/sais.go
@@ -31,15 +31,54 @@ func CreateLMSSuffix(x string, lmsarr []rune) []int {
 	return lmssuffixes
 }
 
+func ResSA(x string, sa *[]int) {
+	s := make([]int, len(x))
+	for i := range s {
+		s[i] = -1
+	}
+}
+
+func CreateBuckets(x string) map[rune]int {
+	xs := CountSort(x)
+
+	buckets := make(map[rune]int) //create first bucket beggining at 0
+	//create buckets with accumulated values
+	for i, v := range xs {
+		if i == 0 {
+			buckets[v] = 0
+			continue
+		}
+		if v != rune(xs[i-1]) {
+			buckets[v] = i
+		}
+	}
+
+	return buckets
+}
+
+func InductionSortL(x string, sa []int, lms []rune) {
+	for i := 0; i < len(x); i++ {
+		if sa[i] == 0 || sa[i] == -1 {
+			continue
+		}
+
+		j := sa[i] - 1
+		if lms[j] == 'L' {
+			// todo
+		}
+	}
+}
+
 func RecursiveSais(x string, sa []int, alphabetsize int) {
 	if len(x) == alphabetsize {
 		return
 	}
 
-	lms := CreateLMS(x)
-	lmssucc := CreateLMSSuffix(x, lms)
+	//lms := CreateLMS(x)
+	// := CreateLMSSuffix(x, lms)
 
-	buckets := CountSort(x)
+	ResSA(x, &sa)
+	// buckets := CountSort(x)
 
 }
 
